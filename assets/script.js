d4c3c38518f4393e7cd5a880798626e3be368cdc
gleicekelly13 <gleice.kelly22@hotmail.com>
2022-10-10 01:04:06 -0300

    implementação de outras funcionalidades

--- assets/script.js
+++ assets/script.js
@@ -9,14 +9,43 @@ function verificar() {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var gênero = ''
-        if (fsex[0].checked) {
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if(fsex[0].checked) {
             gênero = 'Homem'
+            if(idade >= 0 && idade < 10) {
+                //Criança
+                img.setAttribute('src','menino.png')
+            } else if (idade < 21) {
+                //Jovem
+                img.setAttribute('src','homem-jovem.png')
+            } else if (idade < 60) {
+                //Adulto
+                img.setAttribute('src', 'homem-adulto.png')
+            } else {
+                //idoso
+                setAttribute('src', 'homem-idoso.png')
+            }
         } else if (fsex[1].checked) {
             gênero = 'Mulher'
+            if(idade >= 0 && idade < 10) {
+                //Criança
+                img.setAttribute('src', 'menina.png')
+            } else if (idade < 21) {
+                //Joven
+                img.setAttribute('src', 'mulher-jovem.png')
+            } else if (idade < 60) {
+                //Adulto
+                img.setAttribute('src', 'mulher adulta.png')
+            } else {
+                //Idoso
+                img.setAttribute('src', 'mulher idosa.png')
+            }
         }
+
         res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${gênero} com ${idade} anos`
-
-        /* var img = document.createElement('img') */
+        res.appendChild(img)
+        
     }
 }
