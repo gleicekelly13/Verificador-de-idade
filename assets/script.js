e31a4d33c62e3fca8ce070a00af76c8555837457
gleicekelly13 <gleice.kelly22@hotmail.com>
2022-10-10 23:07:55 -0300

    Adicionando elemento para exibir imagem

--- assets/script.js
+++ assets/script.js
@@ -10,41 +10,41 @@ function verificar() {
         var idade = ano - Number(fano.value)
         var gênero = ''
         var img = document.createElement('img')
-        img.setAttribute('id', 'foto')
+        img.setAttribute('id','foto')
         if(fsex[0].checked) {
             gênero = 'Homem'
             if(idade >= 0 && idade < 10) {
                 //Criança
-                img.setAttribute('src','menino.png')
-            } else if (idade < 21) {
+                img.setAttribute('src','assets/images/menino.png')
+            } else if (idade < 35) {
                 //Jovem
-                img.setAttribute('src','homem-jovem.png')
+                img.setAttribute('src','assets/images/homem jovem.png')
             } else if (idade < 60) {
                 //Adulto
-                img.setAttribute('src', 'homem-adulto.png')
+                img.setAttribute('src', 'assets/images/homem adulto.png')
             } else {
                 //idoso
-                setAttribute('src', 'homem-idoso.png')
+                img.setAttribute('src', 'assets/images/homem idoso.png')
             }
         } else if (fsex[1].checked) {
             gênero = 'Mulher'
             if(idade >= 0 && idade < 10) {
                 //Criança
-                img.setAttribute('src', 'menina.png')
-            } else if (idade < 21) {
+                img.setAttribute('src', 'assets/images/menina.png')
+            } else if (idade < 32) {
                 //Joven
-                img.setAttribute('src', 'mulher-jovem.png')
+                img.setAttribute('src', 'assets/images/mulher jovem.png')
             } else if (idade < 60) {
                 //Adulto
-                img.setAttribute('src', 'mulher adulta.png')
+                img.setAttribute('src', 'assets/images/mulher adulta.png')
             } else {
                 //Idoso
-                img.setAttribute('src', 'mulher idosa.png')
+                img.setAttribute('src', 'assets/images/mulher idosa.png')
             }
         }
 
         res.style.textAlign = 'center'
-        res.innerHTML = `Detectamos ${gênero} com ${idade} anos`
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos<br>`
         res.appendChild(img)
         
     }
